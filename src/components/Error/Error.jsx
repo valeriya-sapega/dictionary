2a1921d36427bc7b5af075445397243a613dce3f
Valeriya Sapega <124445060+valeriya-sapega@users.noreply.github.com>
2024-02-26 12:02:10 +0000

    Update Error.jsx

--- src/components/Error/Error.jsx
+++ src/components/Error/Error.jsx
@@ -1,14 +1,12 @@
 import './Error.css';
 
-const Error = () => {
+const Error = ({ text }) => {
   return (
     <div className="error">
       <h1>😕</h1>
       <h3>No Definitions Found</h3>
       <p>
-        Sorry pal, we couldn't find definitions for the word you were looking
-        for. You can try the search again at later time or head to the web
-        instead.
+        {text || "Sorry pal, we couldn't find definitions for the word you were looking for. You can try the search again at later time or head to the web instead."}
       </p>
     </div>
   );
